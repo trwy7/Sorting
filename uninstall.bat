e8f1c21e3c36e2203666d6e4a98c975393f0749f
trey7658 <47907719+trey7658@users.noreply.github.com>
2022-09-02 20:29:05 -0700

    semi fix for uninstall

--- uninstall.bat
+++ uninstall.bat
@@ -1,5 +1,8 @@
 @echo off
 cd /d "C:\treyapps\Sort"
+echo deleting
+cd
+pause
 rd %appdata%"\Microsoft\Windows\Start Menu\Programs\treyapps\Sort\"
 del /q %appdata%"\Microsoft\Windows\Start Menu\Programs\treyapps\Sort\Sort your files.lnk"
 msg * done

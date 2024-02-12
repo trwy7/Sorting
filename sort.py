ea257319142f0eaf08a5e0a475c37c2c22a51004
Trey <47907719+trwy7@users.noreply.github.com>
2024-02-11 18:22:13 -0700

    Add invalid request handling and debug mode

--- sort.py
+++ sort.py
@@ -1,16 +1,33 @@
+import re
 from tkinter.filedialog import askdirectory, askopenfilenames
 import os, json, glob, requests, time
 import zipfile
+debug = False
 def clear():
     os.system('cls' if os.name == 'nt' else 'clear')
+def windowtitle(title):
+    os.system('title' + title if os.name == 'nt' else 'echo -e "\033]0;' + title + '\007"')
+    return os.name == 'nt'
 try:
     with open("filetypes.json", "r") as f:
         filetypes = dict(json.load(f))
 except:
-    print('Getting most recent filetypes. Please wait...')
-    response = requests.get('https://raw.githubusercontent.com/trwy7/Sorting/main/filetypes.json')
-    filetypes = dict(json.loads(response.text))
-
+    try:
+        print('Getting most recent filetypes. Please wait...')
+        response = requests.get('https://raw.githubusercontent.com/trwy7/Sorting/main/filetypes.json')
+        if response.status_code != 200:
+            print('\nFailed to contact github.')
+            exit(2)
+        filetypes = dict(json.loads(response.text))
+    except (json.decoder.JSONDecodeError, TypeError):
+        print('\nGot invalid response, please try again later.')
+        exit(2)
+    except requests.exceptions.RequestException:
+        print('\nFailed to contact github.')
+        exit(2)
+    except:
+        print('\nAn unknown error occurred, try again later.')
+        exit(2)
 
 def folder():
     sortdir = askdirectory(title='Starting directory')
@@ -30,9 +47,12 @@ def sort(files):
     global zipcons
     zipcons = 0
     clear()
-    for file in files:
-        print(f'found {file}')
-    print('\n\nMost files above will be moved from its folder, and cannot be moved back. Please make sure it is correct. Total files moved will be calculated next.')
+    if debug:
+        for file in files:
+            print(f'found {file}')
+        print('\n\nMost files above will be moved from its folder, and cannot be moved back. Please make sure it is correct. Total files moved will be calculated next.')
+    else:
+        print('All files selected will be moved from its folder, and cannot be moved back. Please make sure it is correct. Total files moved will be calculated next.')
     choice = input('(Y/N)> ')
     if choice == 'Y' or choice == 'y':
         print('\n\nDo you allow this program to temporarily look through compressed files. No data will be sent to a server, it is all local.')
@@ -45,8 +65,24 @@ def sort(files):
                     ziptypes = dict(json.load(f))
             except:
                 print('Getting most recent ziptypes. Please wait...')
-                response = requests.get('https://raw.githubusercontent.com/trwy7/Sorting/main/ziptypes.json')
-                ziptypes = dict(json.loads(response.text))
+                try:
+                    response = requests.get('https://raw.githubusercontent.com/trwy7/Sorting/main/ziptypes.json')
+                    if response.status_code != 200:
+                        print('\nFailed to contact github, currently skipping...')
+                        zipcons = 0
+                    ziptypes = dict(json.loads(response.text))
+                except (json.decoder.JSONDecodeError, TypeError):
+                    print('Invalid response, please try again later, currently skipping...')
+                    zipcons = 0
+                    input('Press enter to continue')
+                except requests.exceptions.RequestException:
+                    print('Failed to contact github, currently skipping...')
+                    zipcons = 0
+                    input('Press enter to continue')
+                except:
+                    print('An unknown error occurred, currently skipping...')
+                    zipcons = 0
+                    input('Press enter to continue')
         print('Running test run...')
         global testrun
         testrun = dict()
@@ -64,7 +100,8 @@ def sort(files):
                     totalamnt = 1
                 for key, value in filetypes.items():
                     if file.endswith(key):
-                        print(f'Match: {key} - {file}')
+                        if debug:
+                            print(f'Match: {key} - {file}')
                         actvalue = value
                         try:
                             srtamnt = srtamnt + 1
@@ -81,6 +118,8 @@ def sort(files):
                                     if testfile in filezip.namelist():
                                         actvalue = endpath
                                         break
+                            if not filezip == None:
+                                filezip.close()
                         try:
                             testrun[actvalue] = testrun[actvalue] + 1
                         except KeyError:
@@ -104,29 +143,27 @@ def sort(files):
                 raise KeyError("Nothing was chosen")
             for file in files:
                 if os.path.isfile(file):
-                    try:
-                        totalamnt = totalamnt + 1
-                    except KeyError:
-                        totalamnt = 1
                     for key, value in filetypes.items():
                         if file.endswith(key):
                             a = file.replace('\\','/')
                             b = a.split('/')
                             c = b[-1]
-                            print(f'Moving: {c}')
+                            if debug:
+                                print(f'Moving: {c}')
                             actvalue = value
-                            filezip = None
-                            try:
-                                filezip = zipfile.ZipFile(file)
-                            except zipfile.BadZipFile:
-                                pass
-                            if not filezip == None:
-                                for testfile, endpath in ziptypes.items():
-                                    if testfile in filezip.namelist():
-                                        actvalue = endpath
-                                        break
-                            if not filezip == None:
-                                filezip.close()
+                            if zipcons == 1:
+                                filezip = None
+                                try:
+                                    filezip = zipfile.ZipFile(file)
+                                except zipfile.BadZipFile:
+                                    pass
+                                if not filezip == None:
+                                    for testfile, endpath in ziptypes.items():
+                                        if testfile in filezip.namelist():
+                                            actvalue = endpath
+                                            break
+                                if not filezip == None:
+                                    filezip.close()
                             if not os.path.isdir(movedir + '/' + actvalue):
                                 os.makedirs(movedir + '/' + actvalue)
                             os.rename(a, movedir + '/' + actvalue + c)
@@ -134,6 +171,8 @@ def sort(files):
                             
     else:
         exit()
+
+windowtitle('Welcome!')
 clear()
 print('Which sorting method would you like to use?')
 print('(1) Pick a folder (takes longer to process but recommended)')
@@ -145,6 +184,27 @@ if choice == '1':
 elif choice == '2':
     filesa = files()
     sort(filesa)
+elif choice == 'debug':
+    debug = True
+    print('Debug mode enabled')
+    windowtitle('Welcome! (Debug mode)')
+    clear()
+    print('Which sorting method would you like to use?')
+    print('(1) Pick a folder (takes longer to process but recommended)')
+    print('(2) Manually pick files')
+    choice = input('> ')
+    if choice == '1':
+        filesa = folder()
+        sort(filesa)
+    elif choice == '2':
+        filesa = files()
+        sort(filesa)
+    elif choice == 'debug':
+        print('Please restart the program and try again.')
+        exit(1)
+    else:
+        print('invalid response, quitting')
+        exit(1)
 else:
     print('invalid response, quitting')
     exit(1)
